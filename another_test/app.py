51addadd809bcd5e488330db4d2144e0ea881c67
Jose Capurro <jcapurro@paraguaybox.com>
2018-12-27 18:17:47 -0300

    change

--- another_test/app.py
+++
@@ -1,4 +0,0 @@
-# uwsgi requires the WSGI entry point to be named "application"
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hell-o World!']
